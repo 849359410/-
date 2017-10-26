93789b6f7c0881dd72c2d68960a6fc22e562705f
zhouhua <BruceZhou163@outlook.com>
2017-10-26 17:35:13 +0800

    利用jquery实现放大镜效果

--- src/components/site/goodsinfo.vue
+++ src/components/site/goodsinfo.vue
@@ -21,6 +21,34 @@
                 <div class="goods-box clearfix">
                     <!--商品图片-->
                     <div class="pic-box">
+                        <div class="magnifier" id="magnifier1">
+    <div class="magnifier-container">
+        <div class="images-cover"></div>
+        <!--当前图片显示容器-->
+        <div class="move-view"></div>
+        <!--跟随鼠标移动的盒子-->
+        </div>
+        <div class="magnifier-assembly">
+        <div class="magnifier-btn">
+                <span class="magnifier-btn-left">&lt;</span>
+                <span class="magnifier-btn-right">&gt;</span>
+            </div>
+            <!--按钮组-->
+            <div class="magnifier-line">
+                <ul class="clearfix animation03">
+                    <li v-for="item in ginfo.imglist" key="item.id">
+                        <div class="small-img">
+                            <img :src="item.original_path" />
+                        </div>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!--缩略图-->
+        </div>
+        <div class="magnifier-view"></div>
+        <!--经过放大的图片显示容器-->
+    </div>
 
                     </div>
                     <!--/商品图片-->
@@ -176,6 +204,15 @@
 <script>
     import Affix from 'iview/src/components/affix';
 
+    // 导入jquery插件文件
+    import '../../../statics/site/js/jqplugins/imgzoom/magnifier.js';
+
+    $(function() {
+        $('#magnifier1').imgzoon({
+            magnifier: '#magnifier1'
+        });
+    });
+
     export default {
         components: {
             Affix
@@ -216,5 +253,7 @@
     }
 </script>
 <style scoped>
-
+    /*导入jquery组件的css样式*/
+    
+    @import url('../../../statics/site/js/jqplugins/imgzoom/css/magnifier.css');
 </style>
